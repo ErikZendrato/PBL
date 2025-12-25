8fe93c730377389b25e40ff4f0d2dc4400fa6d07
ErikZendrato <laurenzendrato18@gmail.com>
2025-12-25 18:00:27 +0800

    Ini laravel

---
+++ database/migrations/2025_12_15_042330_create_pembayarans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pembayarans', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('pesanan_id')
+                ->constrained('pesanans')
+                ->onDelete('cascade');
+
+            $table->string('metode');
+            $table->date('tanggal_bayar');
+            $table->decimal('jumlah_bayar', 12, 2);
+            $table->string('status_bayar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pembayarans');
+    }
+};
---
+++ database/migrations/2025_12_15_042355_create_tikets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tikets', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('pembayaran_id')
+                ->constrained('pembayarans')
+                ->onDelete('cascade');
+
+            $table->foreignId('petugas_id')
+                ->constrained('petugas')
+                ->onDelete('cascade');
+
+            $table->string('kode_tiket')->unique();
+            $table->date('tanggal_berlaku');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tikets');
+    }
+};
